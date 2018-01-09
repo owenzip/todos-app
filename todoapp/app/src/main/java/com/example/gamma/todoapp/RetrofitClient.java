88b156e0772367fca6b7be8b6c9dda33eda4917e
Phung Nguyen <phung.nguyen@edge-works.net>
2018-01-09 15:07:43 +0700

    config retrofit.

--- todoapp/app/src/main/java/com/example/gamma/todoapp/RetrofitClient.java
+++ todoapp/app/src/main/java/com/example/gamma/todoapp/RetrofitClient.java
@@ -5,7 +5,16 @@
  */
 package com.example.gamma.todoapp;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /*Class configure Retrofit*/
@@ -13,10 +22,24 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String baseUrl) {
-        if (retrofit==null) {
+        if (retrofit == null) {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+            Gson gson = gsonBuilder.create();
+
+            OkHttpClient.Builder client = new OkHttpClient.Builder();
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            client.addInterceptor(interceptor);
+            client.readTimeout(30, TimeUnit.SECONDS);
+            client.connectTimeout(30, TimeUnit.SECONDS);
+            OkHttpClient okHttpClient = client.build();
+
             retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
